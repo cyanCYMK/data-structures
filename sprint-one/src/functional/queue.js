454e05367ecb4f2fb9865141c7c114703b612291
cyancymk <cylee4@gmail.com>
2014-12-10 15:59:25 -0800

    Completed the functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var topCounter = 0;
+  var botCounter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[topCounter] = value;
+    topCounter++;
   };
 
   someInstance.dequeue = function(){
+    if (someInstance.size() > 0){
+      var temp = storage[botCounter];
+      delete storage[botCounter];
+      botCounter++;
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return topCounter - botCounter;
   };
 
   return someInstance;
