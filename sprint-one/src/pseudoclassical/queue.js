50058b2cda5b218203e21c800e709ab5ccb9c192
cyancymk <cylee4@gmail.com>
2014-12-12 12:21:03 -0800

    Completed linkedList

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.topCounter = 0;
+  this.botCounter = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.topCounter] = value;
+  this.topCounter++;
+}
 
+Queue.prototype.dequeue = function() {
+  if (this.topCounter > this.botCounter) {
+    var temp = this.storage[this.botCounter];
+    delete this.storage[this.botCounter];
+    this.botCounter++;
+    return temp;
+  }
+}
+
+Queue.prototype.size = function() {
+  return this.topCounter - this.botCounter;
+}
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,51 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // in the beginning when list is empty check to see if tail exists
+    // add a next to the current tail
+
+    // take in a value
+    // do something with the node
+    // add that value to the end of the list
+
+    var newNode = Node(value);
+    if (list.tail !== null){
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+    if (list.head === null) {
+      list.head = list.tail;
+    }
+
   };
 
   list.removeHead = function(){
+    // removes first node from the list and returns its value
+    // should delete first node
+    var temp = list.head.value;
+    if (list.head.next !== null) {
+      list.head = list.head.next;
+    }
+    return temp;
+
+
   };
 
   list.contains = function(target){
+    // start from head
+    // see if next.value equals target
+    var search = function (node, target) {
+      if (node.value === target) {
+        return true;
+      }
+      else if (node.next !== null) {
+        return search(node.next, target);
+      }
+      else {
+        return false;
+      }
+    }
+    return search(list.head, target)
   };
 
   return list;
