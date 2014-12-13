6b171930e75630dbe35ea5bb41204cfe4329e143
cyancymk <cylee4@gmail.com>
2014-12-12 20:31:37 -0800

    Implemented set and tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -41,4 +41,6 @@ treeMethods.contains = function(target){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild has constant time complexity
+ * contains has linear time complexity
  */
