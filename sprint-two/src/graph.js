57bcc9f5764bdd0cea265577dab518c999caaf1c
cyancymk <cylee4@gmail.com>
2014-12-12 21:00:52 -0800

    Implemented graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,28 +1,52 @@
 
 
 var Graph = function(){
+  this.storage = {};
+
 };
 
 Graph.prototype.addNode = function(node){
+  var newNode = new Node(node);
+  this.storage[node] = newNode;
 };
 
 Graph.prototype.contains = function(node){
+  if (this.storage[node] !== undefined){
+    return true;
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.storage[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  if (this.storage[fromNode].edges[toNode] === toNode){
+    return true;
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.storage[fromNode].edges[toNode] = toNode;
+  this.storage[toNode].edges[fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  // test does not actually perform this function???
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this.storage){
+    cb(this.storage[key].value);
+  }
 };
+
+var Node = function(value) {
+  this.value = value;
+  this.edges = {};
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
