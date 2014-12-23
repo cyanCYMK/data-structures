857954f632b75c14f6852bd6647e3b08d67e54b4
Christine <cylee4@gmail.com>
2014-12-23 00:35:23 -0800

    Implement binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,58 @@
 var BinarySearchTree = function(value){
+  var binSrchTree = {};
+  binSrchTree.value = value;
+  binSrchTree.left = null;
+  binSrchTree.right = null;
+  _.extend(binSrchTree, BSTMethods);
+
+  return binSrchTree;
+};
+
+var BSTMethods = {};
+BSTMethods.insert = function(value){
+  if( value < this.value ){
+    if( this.left === null ){
+      this.left = BinarySearchTree(value);
+    }
+    this.left.insert(value);
+  }
+  if( value > this.value ){
+    if( this.right === null ){
+      this.right = BinarySearchTree(value);
+    }
+    this.right.insert(value);
+  }
+};
+BSTMethods.contains = function(value){
+  if( this.value === value ){
+    return true;
+  }
+  if( value < this.value ){
+    if( this.left === null ){
+      return false;
+    }
+    return this.left.contains(value);
+  }
+  if( value > this.value ){
+    if( this.right === null ){
+      return false;
+    }
+    return this.right.contains(value);
+  }
+  return false;
+
+};
+BSTMethods.depthFirstLog = function(callback){
+  if( this.value === null ){
+    return;
+  }
+  callback(this.value);
+  if( this.left !== null ){
+    this.left.depthFirstLog(callback);
+  }
+  if( this.right !== null ){
+    this.right.depthFirstLog(callback);
+  }
 };
 
 
